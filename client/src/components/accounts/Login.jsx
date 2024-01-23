9fe55543aae4e549daf65670969f7197ca43b457
Sayantan Mondal <95515582+FakespideY@users.noreply.github.com>
2024-01-23 17:32:14 +0530

    Initial commit

---
+++ client/src/components/accounts/Login.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { Box, TextField, Button, Typography, styled } from "@mui/material";
+
+//styled components
+const Component = styled(Box)`
+  width: 400px;
+  margin: auto;
+  box-shadow: 5px 2px 5px 2px rgba(0 0 0/ 0.6);
+`;
+
+const Image = styled("img")({
+  width: 100,
+  margin: "auto",
+  display: "flex",
+  padding: "50px 0 0",
+});
+
+const Wrapper = styled(Box)`
+  padding: 25px 35px;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  & > div,
+  & > button,
+  & > p {
+    margin-top: 20px;
+  }
+`;
+const LoginButton = styled(Button)`
+  text-transform: none;
+  background: #fb641b;
+  color: #fff;
+  height: 48px;
+  border-radius: 2px;
+`;
+const Text = styled(Typography)`
+  color: #878787;
+  font-size: 16px;
+`;
+
+const SignupButton = styled(Button)`
+  text-transform: none;
+  background: #fff;
+  color: #2874f0;
+  height: 48px;
+  border-radius: 2px;
+  box-shadow: 0 2px 4px 0 rgba(0 0 0/20%);
+`;
+
+const login = () => {
+  const imageURL =
+    "https://www.sesta.it/wp-content/uploads/2021/03/logo-blog-sesta-trasparente.png";
+
+  return (
+    <Component>
+      <Box>
+        <Image src={imageURL} alt="Login" />
+        {/* <Wrapper>
+          <TextField label="Enter username" variant="standard" />
+          <TextField label="Enter password" variant="standard" />
+          <LoginButton variant="contained">Login</LoginButton>
+          <Text style={{ textAlign: "center" }}>OR</Text>
+          <SignupButton>Create an account</SignupButton>
+        </Wrapper> */}
+
+        <Wrapper>
+          <TextField label="Enter Name" variant="standard" />
+          <TextField label="Enter Email" variant="standard" />
+          <TextField label="Enter password" variant="standard" />
+          <SignupButton>Signup</SignupButton>
+          <Text style={{ textAlign: "center" }}>OR</Text>
+          <LoginButton variant="contained">Already have an account</LoginButton>
+        </Wrapper>
+      </Box>
+    </Component>
+  );
+};
+
+export default login;
